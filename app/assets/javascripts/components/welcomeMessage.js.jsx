a4e388caea7e28a94ca56e06dda98eb042b8321a
Jared <jared.kimball.f@gmail.com>
2016-01-01 17:59:50 -0500

    Added a state example

--- app/assets/javascripts/components/welcomeMessage.js.jsx
+++ app/assets/javascripts/components/welcomeMessage.js.jsx
@@ -1,12 +1,31 @@
 var HelloMessage = React.createClass({
+
+  getInitialState: function() {
+  	return {
+  		signedIn: this.props.signedIn
+  	};
+  },
+  signIn: function() {
+  	this.setState({ signedIn: "true" });
+  },
+  signOut: function() {
+  	this.setState({ signedIn: "false" });
+  },
   render: function() {
-    return <div>Hello {this.props.name}</div>;
+    return (
+    	<div>Hello {this.props.name}. Signed in {this.state.signedIn} 
+    		<button onClick={this.signIn}>Sign In</button>
+    		<button onClick={this.signOut}>Sign Out</button>
+    	</div>
+    );
   }
 });
 
+
+
 var ready = function () {
   ReactDOM.render(
-    <HelloMessage name="John" />,
+    <HelloMessage name="Jared" signedIn="false" />,
     document.getElementById('app')
   );
 };
