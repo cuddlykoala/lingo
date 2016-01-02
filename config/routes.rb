03aa2d0a52166e26eace05957602dad861eb41bd
Jared <jared.kimball.f@gmail.com>
2016-01-01 23:34:22 -0500

    Added simple authentication. Added logged-in page view (take quiz, add cards).

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users 
+
+  devise_scope :user do
+  	get 'login', to: "devise/sessions#new"
+  	get 'signup', to: "devise/registrations#new"
+  end
+
+
+
   root 'home#index'
 end
