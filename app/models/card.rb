6e100a884b79b95b44286786266824c24cb77c50
Jared <jared.kimball.f@gmail.com>
2016-01-03 01:11:24 -0500

    Added daily, weekly, monthly filters

--- app/models/card.rb
+++ app/models/card.rb
@@ -13,6 +13,26 @@ class Card < ActiveRecord::Base
 
   before_create :set_default_points
 
+  scope :between_points, ->(min, max) {
+  	where(points: min..max)
+  }
+
+  def self.daily(user)
+  	self.between_points(0, 12).where(user: user)
+  end
+
+  def self.weekly(user)
+  	self.between_points(12, 24).where(user: user)
+  end
+
+  def self.monthly(user)
+  	self.between_points(24, 100).where(user: user)
+  end
+
+  def increment_score
+  	points += 3
+  end
+
   private
 
   def set_default_points
