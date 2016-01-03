47ff77ae59d854e7c4a8d352d9892075e822e756
Jared <jared.kimball.f@gmail.com>
2016-01-03 02:24:10 -0500

    Added user methods for calling card piles (d, w, monthly)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,20 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :cards
+
   has_many :words,
   			through: :cards
-  			
+
+  def daily_cards
+  	Card.daily(self)
+  end
+
+  def weekly_cards
+  	Card.weekly(self)
+  end
+
+  def monthly_cards
+  	Card.monthly(self)
+  end
+
 end
